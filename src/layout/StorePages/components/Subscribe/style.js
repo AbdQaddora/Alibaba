f464443f944426aeb30893e67a13cdd756da9f12
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2023-02-01 21:57:58 +0200

    add margin to the subscriber section

--- src/layout/StorePages/components/Subscribe/style.js
+++ src/layout/StorePages/components/Subscribe/style.js
@@ -3,10 +3,12 @@ import styled from "styled-components";
 const Style = styled.div`
     background-color: ${props => props.theme.colors.gray.gray200};
     padding: 38px 16px;
+    margin-top: 20px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
     .subscribe__form{
         display: flex;
         gap: 8px;
